0d866ecb93a1b28e36bf4733632305e29f75949f
Denis <denproko@yandex.ru>
2021-02-03 02:53:38 +0300

    Initial Commit

---
+++ Hello world/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Hello world
+//
+//  Created by Денис Прокофьев on 03.02.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
